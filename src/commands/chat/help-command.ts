fcd7e9ab100e30b8f8cf86bfaafde16b8638129a
delphyinium <tiger.roar@live.com>
2023-04-15 19:42:47 -0400

    added time_cmd to embedbuilder

--- src/commands/chat/help-command.ts
+++ src/commands/chat/help-command.ts
@@ -38,6 +38,12 @@ export class HelpCommand implements Command {
                             Lang.getRef('chatCommands.info', Language.Default)
                         )
                     ),
+                    CMD_LINK_TIME: FormatUtils.commandMention(
+                        await ClientUtils.findAppCommand(
+                            intr.client,
+                            Lang.getRef('chatCommands.time', Language.Default)
+                        )
+                    ),
                 });
                 break;
             }
