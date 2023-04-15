9687802721ed7304d3507906bb47da79470c6e3e
delphyinium <tiger.roar@live.com>
2023-04-14 21:35:07 -0400

    started work on time command

---
+++ src/commands/chat/time-command.ts
@@ -0,0 +1,19 @@
+import { ChatInputCommandInteraction, PermissionsString } from 'discord.js';
+import { RateLimiter } from 'discord.js-rate-limiter';
+
+import { Language } from '../../models/enum-helpers/index.js';
+import { Lang } from '../../services/index.js';
+import { InteractionUtils } from '../../utils/index.js';
+import { Command, CommandDeferType } from '../index.js';
+
+export class TimeCommand implements Command {
+    public names = [Lang.getRef('chatCommands.time', Language.Default)];
+    public cooldown = new RateLimiter(1, 5000);
+    public deferType = CommandDeferType.PUBLIC;
+    public requireClientPerms: PermissionsString[] = [];
+
+    public async execute(intr: ChatInputCommandInteraction): Promise<void> {
+        const currentTime = new Date().toLocaleTimeString();
+        await InteractionUtils.send(intr, `The current local time is: ${currentTime}`);
+    }    
+}
